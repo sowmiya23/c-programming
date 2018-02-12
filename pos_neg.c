1b0f022fef83f5d71f02f8e9bb0230ffd31e7cc1
sowmiya23 <35104349+sowmiya23@users.noreply.github.com>
2018-02-12 21:35:02 +0530

    Create pos_neg.c

---
+++ pos_neg.c
@@ -0,0 +1,18 @@
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+	printf("\npositive");
+	}
+	else if(n==0)
+	{
+	printf("\nzero");
+	}
+	else
+	{
+	printf("\nnegative");
+	}
+	return 0;
+}
